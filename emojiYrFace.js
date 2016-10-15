a085e9d5d32f568bbbb65a7373480af4dd873881
rebleo <rml444@nyu.edu>
2016-10-14 22:18:25 -0400

    getting clm to work w/ image

--- emojiYrFace.js
+++ emojiYrFace.js
@@ -1,16 +1,9 @@
-var socket = io.connect('https://rml444.itp.io:8085/');
+// var socket = io.connect('https://rml444.itp.io:8085/');
 //
-// socket.on('they', function (data, clients) {
-// 	for (var i = 0; i < clients.length; i++){
-// 			// var pix = new makeTheyFace(data);
-// 		theyFace = document.getElementById('themImage').innerHTML += data;
-// 			}
-//
-// 	});
 
-	socket.on('they', function (data) {
-			theyFace = document.getElementById('themImage').innerHTML += data;
-				});
+	// socket.on('they', function (data) {
+	// 		theyFace = document.getElementById('themImage').innerHTML += data;
+	// 			});
 
 
 // socket.on('start', function() {
@@ -20,7 +13,11 @@ var socket = io.connect('https://rml444.itp.io:8085/');
 var thecontext;
 var thecanvas;
 var theyFace;
-// var emoji = new Image();
+var meVideo;
+var emoji = new Image();
+//variable for ctracker
+var ctracker;
+var emojis = [];
 
 
 var emojiGo = function() {
@@ -29,52 +26,60 @@ var emojiGo = function() {
 	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
 	// The video element on the page to display the webcam
-	var meVideo = document.getElementById('selfieVideo');
-	//###########
-	// var themVideo = document.getElementById('themImage');
+	meVideo = document.getElementById('selfieVideo');
 
 	// if we have the method
 	if (navigator.getUserMedia) {
 		navigator.getUserMedia({video: true}, function(stream) {
 				meVideo.src = window.URL.createObjectURL(stream) || stream;
-				meVideo.play();
+				// meVideo.play();
+
+				draw();
 			}, function(error) {alert("Failure " + error.code);});
 	}
 
 ////////////// make the page //////////////////
-	// emoji.src = '/24.png';
+
+	emoji.src = '/24.png';
+
 	thecanvas = document.getElementById('thecanvas');
  	thecontext = thecanvas.getContext('2d');
 	thecontext.beginPath();
 	thecontext.rect(0, 0, innerWidth, innerHeight);
-	thecontext.fillStyle = "red";
+	thecontext.fillStyle = "green";
 	thecontext.fill();
+
+
+	ctracker = new clm.tracker();
+	ctracker.init(pModel);
+	ctracker.start(meVideo);
+
 
 } // end of emojiG0 == init()
 //
-// function makeTheyFace(source, elementId){
-// 	this.pix = new Image();
-// 	this.source = source;
-// 	this.pix.src = source;
-// 	this.elementId = elementId;
-// }
 
 var draw = function() {
 		//EMOJI YR FACE
 	// thecontext.drawImage(emoji, 0, 0, 100, 100);
-	thecontext.drawImage(meVideo,100,100,meVideo.width,meVideo.height);
-	thecontext.drawImage(theyFace,300,100);
+	thecontext.drawImage(meVideo,25,25,meVideo.width,meVideo.height);
+	// thecontext.drawImage(theyFace,300,100);
 
 
 ///whatever the canvas state is at this point will be sent over the network
- var dataUrl = thecanvas.toDataURL('image/webp', 1);
+ // var dataUrl = thecanvas.toDataURL('image/webp', 1);
  // 	selfiimage.thecontext(size,size) ---> pu thtat in the dataURL...
-	meVideo.src = dataUrl;
- 	socket.emit('they', dataUrl);
+
 	//draw every 6 seconds
- setTimeout(draw,6000);
+ setTimeout(draw,100);
+   var positions = ctracker.getCurrentPosition();
+	//  console.log(positions);
+	 if (positions.length > 0) {
+		 for (var i = 0; i < 1; i++) {
+				thecontext.drawImage(emoji,positions[i][0],positions[i][1]);
+			}
+		}
 }
 
-draw();
+
 
 window.addEventListener('load', emojiGo);
