1291c36498d836c9e42db56013e6cd8b846b9c81
rebleo <rml444@nyu.edu>
2016-10-18 00:22:29 -0400

    need to figure out feeling emojis

--- emojiYrFace.js
+++ emojiYrFace.js
@@ -1,85 +1,132 @@
-// var socket = io.connect('https://rml444.itp.io:8085/');
-//
+//     ( emoji ourFace )
+//a chat room for two Emoticons
+//https://rml444.itp.io:8085/index.html
+//a programming exercise by rebecca (marks) leopold, 2016
+//thx Shawn Van Every && Rubin Huang && Nicole He
+////also thx zac, kevin, jasmine, aaron for letting me "share" yr idea.
+//http://www.potatoyourface.com/
+//using clmtrackr - https//github.com/auduno/clmtrackr
 
-	// socket.on('they', function (data) {
-	// 		theyFace = document.getElementById('themImage').innerHTML += data;
-	// 			});
+var socket = io.connect('');
 
-
-// socket.on('start', function() {
-// 			console.log("emoji go!");
-// });
-
-var thecontext;
-var thecanvas;
+var theCanvas;
+var theContxt;
+var emos = [];
 var theyFace;
-var meVideo;
-var emoji = new Image();
-//variable for ctracker
-var ctracker;
-var emojis = [];
+var meFace;
+// var theyData;
+var emoji = new Image();//
 
+var ec = new emotionClassifier();
+ec.init(emotionModel);
+var emotionData = ec.getBlank();
+
+socket.on('connect', function() {
+				console.log("Connected");
+			});
+
+//recieve their image as dataurl
+ socket.on('they', function (theyData) {
+	//  console.log("it's them!");
+		theyFace = document.getElementById('themImage');
+		theyFace.src = theyData;
+		// console.log(theyData);
+
+});
 
 var emojiGo = function() {
+	console.log("loaded");
+  // ctrack.init(pModel);
 /////// communication / compatibility jig /////
 	window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
 	// The video element on the page to display the webcam
-	meVideo = document.getElementById('selfieVideo');
+	meFace = document.getElementById('selfieVideo');
 
 	// if we have the method
 	if (navigator.getUserMedia) {
 		navigator.getUserMedia({video: true}, function(stream) {
-				meVideo.src = window.URL.createObjectURL(stream) || stream;
-				// meVideo.play();
-
+				meFace.src = window.URL.createObjectURL(stream) || stream;
+				meFace.play();
+				//call draw after get video info
 				draw();
+
 			}, function(error) {alert("Failure " + error.code);});
 	}
 
 ////////////// make the page //////////////////
-
 	emoji.src = '/24.png';
+//create the canvas make it green
+	theCanvas = document.getElementById('thecanvas1');
+ 	theContxt = theCanvas.getContext('2d');
 
-	thecanvas = document.getElementById('thecanvas');
- 	thecontext = thecanvas.getContext('2d');
-	thecontext.beginPath();
-	thecontext.rect(0, 0, innerWidth, innerHeight);
-	thecontext.fillStyle = "green";
-	thecontext.fill();
+	var theTitleCanvas = document.getElementById('thecanvas2');
+	var theTitleText = theTitleCanvas.getContext('2d');
 
+	theTitleText.font="20px Georgia";
+	theTitleText.fillText("😶",75,20);
 
+//push the emojis into the emos array
+	var emo1 = document.getElementById('icon1');
+	var emo2 = document.getElementById('icon1');
+	var emo3 = document.getElementById('icon1');
+	var emo4 = document.getElementById('icon1');
+	var emo5 = document.getElementById('icon1');
+ 	emos.push(emo1,emo2,emo3,emo4,emo5);
+
+ // 	console.log(emos);
+
+//init the CLM tracker
 	ctracker = new clm.tracker();
 	ctracker.init(pModel);
-	ctracker.start(meVideo);
+	ctracker.start(meFace);
+
 
 
 } // end of emojiG0 == init()
-//
+
 
 var draw = function() {
-		//EMOJI YR FACE
-	// thecontext.drawImage(emoji, 0, 0, 100, 100);
-	thecontext.drawImage(meVideo,25,25,meVideo.width,meVideo.height);
-	// thecontext.drawImage(theyFace,300,100);
+//ANYTHING DRAWN ON CANVAS WILL BE EMITTED YOU FOOL!
+ 	theContxt.drawImage(meFace,0,0,meFace.width,meFace.height);
 
+//all this clm tracker emotion stuff i don't entirely understand
+	var cp = ctracker.getCurrentParameters();
+	 //angry, disgusted, fear, sad, suprised, happy
+	var er = ec.meanPredict(cp);
+	var positions = ctracker.getCurrentPosition();
 
-///whatever the canvas state is at this point will be sent over the network
- // var dataUrl = thecanvas.toDataURL('image/webp', 1);
- // 	selfiimage.thecontext(size,size) ---> pu thtat in the dataURL...
+	// theContxt.drawImage(emos[0], 100,100);
+  // console.log(positions);
+	//for the position of yr face
+	if (positions.length > 0) {
 
-	//draw every 6 seconds
- setTimeout(draw,100);
-   var positions = ctracker.getCurrentPosition();
-	//  console.log(positions);
-	 if (positions.length > 0) {
-		 for (var i = 0; i < 1; i++) {
-				thecontext.drawImage(emoji,positions[i][0],positions[i][1]);
+		if (er) {
+//this part needs to be cleaned up/fixed
+			for (var i = 0; i < 1; i++) {
+				for (var j = 0; j < er.length; j++){
+						if (er[j].value > 0.4) {
+							document.getElementById('icon'+(j+1)).style.visibility = 'visible';
+							theContxt.drawImage(emos[j+1],positions[i][0], positions[i][1]-75);
+					// theContxt.drawImage(emos[j+1], 100,100)
+					} else {
+						document.getElementById('icon'+(j+1)).style.visibility = 'hidden';
+				 	theContxt.drawImage(emoji,positions[i][0], positions[i][1]-75);
+				}
 			}
 		}
+
+//draw the hardcoded emoji to the face tracking position
+
+		 }
+	 }
+///whatever the canvas state is at this point will be sent over the network
+ 	var meData = theCanvas.toDataURL('image/jpeg', 0.2);
+
+ // 	socket.emit('canvas1', dataCanvas1);
+	socket.emit('dataMyFace',meData);
+	// draw every 6 seconds
+ 	setTimeout(draw,100);
 }
-
-
-
 window.addEventListener('load', emojiGo);
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-//thx Shawn Van Every && Jason Beck
-
 var https = require('https');
 var fs = require('fs'); // Using the filesystem module
 var url =  require('url');
 
 var options = {
+
   key: fs.readFileSync('my-key.pem'),
   cert: fs.readFileSync('my-cert.pem')
 };
+
+// var clientCount = 0;
+// var clientList = [];
 
 function handleIt(req, res) {
 	var parsedUrl = url.parse(req.url);
@@ -29,6 +31,7 @@ function handleIt(req, res) {
 			// Otherwise, send the data, the contents of the file
 			res.writeHead(200);
 			res.end(fileContents);
+			console.log("connected to rml444")
   		}
   	);
 
@@ -37,7 +40,7 @@ function handleIt(req, res) {
 }
 
 var httpServer = https.createServer(options, handleIt);
-httpServer.listen(8085);
+httpServer.listen(8087);
 
 // WebSocket Portion
 var io = require('socket.io').listen(httpServer);
@@ -47,26 +50,14 @@ io.sockets.on('connection',
 	// We are given a websocket object in our function
 	function (socket) {
 		console.log("We have a new client: " + socket.id);
-		// When this user emits, client side:
 
-	// socket.on("they", function (data) {
-	// 		io.of('/').clients(function(error, clients){
-	// 			if (error) throw error;
-	// 				socket.broadcast.emit("they", data, id, clients);
-	// 		});
-	// 	});
 
-		socket.on('they', function(data) { //this is how the server is catching the data
-				console.log(data);
+		//this is the socket for the incoming image footage
+		socket.on('dataMyFace', function(meData){
+			// console.log("it's me!");
+				socket.broadcast.emit('they', meData);
 
 		});
-		// socket.on('start', function(){
-		// 	socket.broadcast.emit('start');
-		// });
 
-
-		socket.on('disconnect', function() {
-			socket.broadcast.emit('disconnect', socket.id);
-			console.log("Client has disconnected" + socket.id);
-		});
+	
 	});
